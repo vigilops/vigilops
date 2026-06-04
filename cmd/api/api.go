c06c6f3ebd695460ead714a01a59938ccb8f91a1
yusufnuru <115386363+yusufnuru@users.noreply.github.com>
2026-06-05 01:02:37 +0800

    initial commit: scaffold project with go

---
+++ cmd/api/api.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"go.uber.org/zap"
+)
+
+type application struct {
+	config config
+	pool   *pgxpool.Pool
+	logger *zap.SugaredLogger
+}
+
+type config struct {
+	addr string
+	env  string
+	db   dbConfig
+}
+
+type dbConfig struct {
+	addr     string
+	maxConns int32
+}
+
+func (app *application) mount() *chi.Mux {
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/health", app.healthHandler)
+	})
+
+	return r
+}
+
+func (app *application) run(mux http.Handler) error {
+	srv := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      mux,
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		IdleTimeout:  time.Minute,
+	}
+
+	app.logger.Infow("server started", "addr", app.config.addr, "env", app.config.env, "version", version)
+	return srv.ListenAndServe()
+}
